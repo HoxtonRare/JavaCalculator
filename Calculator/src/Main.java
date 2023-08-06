a7263bbe0d755b757fc9ee27671630af6eaa3e57
Ruslan <ruslan24krut@gmail.com>
2023-08-06 19:02:30 +0300

    add Calculator

---
+++ Calculator/src/Main.java
@@ -0,0 +1,94 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String example;
+
+        System.out.print("Введите пример: ");
+        example = in.nextLine();
+        System.out.println("Ответ: " + calc(example));
+    }
+
+    public static String calc(String input){
+        int num1;
+        int num2;
+        int answer = 0;
+        String result;
+        char action = 0;
+        String[] nums = new String[0];
+
+        if (input.contains("+")){
+            action = '+';
+            nums = input.split("\\+");
+        }
+        else if (input.contains("-")){
+            action = '-';
+            nums = input.split("-");
+        }
+        else if (input.contains("*")){
+            action = '*';
+            nums = input.split("\\*");
+        }
+        else if (input.contains("/")){
+            action = '/';
+            nums = input.split("/");
+        }
+        else{
+            try {
+                throw new IOException();
+            } catch (IOException e){
+                System.out.println("Введен неверный пример");
+                System.exit(1);
+            }
+        }
+
+        if (nums.length > 2) {
+            try {
+                throw new IOException();
+            } catch (IOException e) {
+                System.out.println("Введен неверный пример");
+                System.exit(2);
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            nums[i] = nums[i].replaceAll(" ", "");
+        }
+
+        num1 = Integer.parseInt(nums[0]);
+        num2 = Integer.parseInt(nums[1]);
+
+        if (num1 < 0 || num1 > 10 || num2 < 0 || num2 > 10){
+            try {
+                throw new IOException();
+            } catch (IOException e){
+                System.out.println("Введено неверное число");
+                System.exit(3);
+            }
+        }
+
+
+        switch (action){
+            case('+'):
+                answer = num1 + num2;
+                break;
+            case('-'):
+                answer = num1 - num2;
+                break;
+            case('*'):
+                answer = num1 * num2;
+                break;
+            case('/'):
+                answer = num1 / num2;
+                break;
+        }
+
+        result = String.valueOf(answer);
+
+        return result;
+    }
+}
